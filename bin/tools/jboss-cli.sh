6f61f1c090872ca12ff088a6e5c7e0f2dc59d630
ratwater <jbride2001@yahoo.com>
2014-01-31 15:17:11 -0700

    initial

---
+++ bin/tools/jboss-cli.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export JAVA_OPTS="-Djboss.management.client_socket_bind_address=$OPENSHIFT_FSW_IP"
+
+# JA Bride:  use jboss_bpm_soa
+#/usr/share/jbossas/bin/jboss-cli.sh "$@"
+/opt/jboss_bpm_soa/jboss-eap-6.1/bin/jboss-cli.sh "$@"
