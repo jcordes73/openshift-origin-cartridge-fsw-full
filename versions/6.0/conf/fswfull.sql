ab2e9de93d95a37e76891b7bac608f87ef3ca3b9
ratwater <jbride2001@yahoo.com>
2014-02-14 13:50:17 -0700

    latest

--- versions/6.0/conf/fswfull.sql
+++ versions/6.0/conf/fswfull.sql
@@ -1,13 +1,2 @@
-create database if not exists bpel;
-grant all on bpel.* to 'bpel'@'${env.OPENSHIFT_BPMS_IP}' identified by '${env.OPENSHIFT_MYSQL_DB_USERNAME}';
-create database if not exists gs;
-grant all on gs.* to 'gs'@'${env.OPENSHIFT_BPMS_IP}' identified by '${env.OPENSHIFT_MYSQL_DB_USERNAME}';
-create database if not exists jbpm;
-grant all on jbpm.* to 'jbpm'@'${env.OPENSHIFT_BPMS_IP}' identified by '${env.OPENSHIFT_MYSQL_DB_USERNAME}';
-create database if not exists rtgov;
-grant all on rtgov.* to 'rtgov'@'${env.OPENSHIFT_BPMS_IP}' identified by '${env.OPENSHIFT_MYSQL_DB_USERNAME}';
-create database if not exists sramp;
-grant all on sramp.* to 'sramp'@'${env.OPENSHIFT_BPMS_IP}' identified by '${env.OPENSHIFT_MYSQL_DB_USERNAME}';
-
 create database if not exists demo;
 grant all on demo.* to 'demo'@'${env.OPENSHIFT_BPMS_IP}' identified by '${env.OPENSHIFT_MYSQL_DB_USERNAME}';
