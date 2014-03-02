89a729eacb81f73dff34794825101e6181fa3b46
jcordes73 <jcordes@redhat.com>
2014-03-02 18:57:38 +0100

    Update jboss-cli.sh
    Revert to /usr/share/jbossas/bin/jboss-cli.sh on self-installed OSE 2.

--- bin/tools/jboss-cli.sh
+++ bin/tools/jboss-cli.sh
@@ -2,5 +2,5 @@
 export JAVA_OPTS="-Djboss.management.client_socket_bind_address=$OPENSHIFT_FSW_IP"
 
 # JA Bride:  use jboss_bpm_soa
-#/usr/share/jbossas/bin/jboss-cli.sh "$@"
-/opt/jboss_bpm_soa/jboss-eap-6.1/bin/jboss-cli.sh "$@"
+/usr/share/jbossas/bin/jboss-cli.sh "$@"
+#/opt/jboss_bpm_soa/jboss-eap-6.1/bin/jboss-cli.sh "$@"
